3a29527dc3890b2e0af0228a041db0d4efdb06e1
Ahmed Hussain <ahmedhussainnow@gmail.com>
2025-09-16 18:09:11 +0600

    security measures tackled

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAgNhQ3Jez8xJqhC77k5lFqUo7Eekbq49Q",
-  authDomain: "react-fb-auth-c652e.firebaseapp.com",
-  projectId: "react-fb-auth-c652e",
-  storageBucket: "react-fb-auth-c652e.firebasestorage.app",
-  messagingSenderId: "356490335521",
-  appId: "1:356490335521:web:31b270efa8f01f157c5a60",
+  apiKey: `${import.meta.env.VITE_FIREBASE_API_KEY}`,
+  authDomain: `${import.meta.env.VITE_AUTH_ADMIN}`,
+  projectId: `${import.meta.env.VITE_PROJECT_ID}`,
+  storageBucket: `${import.meta.env.VITE_STORAGE_BUCKET}`,
+  messagingSenderId: `${import.meta.env.VITE_MESSAGING_SENDER_ID}`,
+  appId: `${import.meta.env.VITE_APP_ID}`,
 };
 
 // Initialize Firebase
